a24da8a8a1fb58d01d54c455137850b636f27ca0
diarra02 <camaradiarra407@gmail.com>
2025-07-19 16:24:22 +0000

    afficher livrer

--- bibliotheque.h
+++ bibliotheque.h
@@ -5,16 +5,5 @@ typedef struct {
     int isbn;
     int estEmprunte;
 } Livre;
-typedef struct {
-    int isbn;
-    char nom_emprunteur[100];
-    char date[11];
-     char date_retour[11];
-} Emprunt;
-void menu();
 void principal();
-void emprunterLivre();
-void afficherEmprunts(Emprunt[], int);
-void retournerLivre(Livre[], int );
-
-
+void rechercherLivre();
